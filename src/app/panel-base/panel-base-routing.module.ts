4992465aad7d467c3a7662ed5db2525c16056e28
Sebita <sebacardozo888@gmail.com>
2020-06-23 17:11:00 -0300

    bug raro sin no tiene esta direcion chau todo

--- src/app/panel-base/panel-base-routing.module.ts
+++ src/app/panel-base/panel-base-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [
   {
     path: '', component: PanelBaseComponent,
     children: [
+      { path: 'logn', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
       { path: 'lista', loadChildren: () => import('../clientes/clientes.module').then(m => m.ClientesModule) },
       { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule) }
     ]
-  },
-
+  }
 ]
 
 @NgModule({
