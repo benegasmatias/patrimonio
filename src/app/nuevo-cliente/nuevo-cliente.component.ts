c0ef57cd9dce5716a58f30b4d4694771ef721a1c
Gastnzk <gastnsteelix@gmail.com>
2020-06-24 17:13:32 -0300

    Servicio de nuevo componente

--- src/app/nuevo-cliente/nuevo-cliente.component.ts
+++ src/app/nuevo-cliente/nuevo-cliente.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService} from '../login/services/login.service';
-//
+import {NuevoUsuarioService} from './services/nuevo-usuario.service';
 import {HttpClient} from '@angular/common/http';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 //
@@ -11,26 +10,45 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class NuevoClienteComponent implements OnInit {
 
-public login:LoginService;
+public nuevoUsuario:NuevoUsuarioService;
 
-form = new FormGroup({
+public monedas : Option[];
+
+public form = new FormGroup({
   name: new FormControl('',Validators.required),
-  apell: new FormControl('',Validators.required),
+  last_name: new FormControl('',Validators.required),
   username: new FormControl('',Validators.required),
   email: new FormControl('',Validators.required),
   password: new FormControl('',Validators.required),
-  nomAcademia: new FormControl('',Validators.required),
-  monPago: new FormControl('',Validators.required),
+  academia: new FormControl('',Validators.required),
+  moneda: new FormControl('',Validators.required),
 });
 
-  constructor(loginService:LoginService, http:HttpClient) { 
-    this.login=loginService;
+  constructor(nuevou:NuevoUsuarioService, http:HttpClient) { 
+    this.nuevoUsuario=nuevou;
   }
 
   ngOnInit(): void {
+    this.monedas = [
+      {id: 'ARS', description: 'Pesos argentinos (ARS)'}, 
+      {id: 'BOB', description: 'Bolivianos (BOB)'},    
+      {id: 'BRL', description: 'Reales brasileños (BRL)'},    
+      {id: 'CLP', description: 'Pesos chilenos (CLP)'},    
+      {id: 'COP', description: 'Pesos colombianos (COP)'},    
+      {id: 'EUR', description: 'Euros (EUR)'},    
+      {id: 'MXN', description: 'Pesos mexicanos (MXN)'},    
+      {id: 'PEN', description: 'Soles peruanos (PEN)'},    
+      {id: 'PYG', description: 'Guaraníes (PYG)'},    
+      {id: 'USD', description: 'Dólares estadounidenses (USD)'},    
+      {id: 'UYU', description: 'Pesos uruguayos (UYU)'},    
+
+    ];    
   }
   enviar(){
-    //this.login.setUser(this.user)
-    console.log(this.form);
+    this.nuevoUsuario.enviar(this.form.value)
   }
+}
+export interface Option {
+  id: string;
+  description: string;
 }
